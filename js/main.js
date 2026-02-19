9ba9d9546385861b7acf15cd54e6ffe9b0aae42c
Kamura <kamura@example.com>
2026-02-19 13:32:27 +0900

    cleanup: remove unnecessary files

--- js/main.js
+++ js/main.js
@@ -16,12 +16,9 @@ $(function () {
   $('a[href^="#"]').click(function () {
     // クリックしたaタグのリンクを取得
     let href = $(this).attr("href");
-    // ジャンプ先のid名をセット hrefの中身が#もしくは空欄なら,htmlタグをセット
     let target = $(href == "#" || href == "" ? "html" : href);
     // ページトップからジャンプ先の要素までの距離を取得
     let position = target.offset().top;
-    // animateでスムーススクロールを行う   ページトップからpositionだけスクロールする
-    // 600はスクロール速度で単位はミリ秒  swingはイージングのひとつ
     $("html, body").animate({ scrollTop: position }, 600, "swing");
     $("header").toggleClass("open");
     // urlが変化しないようにfalseを返す
@@ -63,8 +60,6 @@ $(function () {
       let target = $(this).offset().top;
       // 画面の高さ
       let windowHeight = $(window).height();
-      // fadeinクラスの要素が画面下にきてから200px通過した
-      // したタイミングで要素を表示
       if (scroll > target - windowHeight + 200) {
         $(this).css("opacity", "1");
         $(this).css("transform", "translateY(0)");
